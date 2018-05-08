1003ddb6acecb6bfc4b3e3ff0afbdf163896fff7
KIKI Cheng <kiki.chengxiaoqi@gmail.com>
2018-05-08 15:35:52 +0800

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var contentIncluder = require('./');
+var rename = require('gulp-rename');
+
+gulp.task('concat',function() {
+    gulp.src("./content.html")
+        .pipe(contentIncluder({
+            includerReg:/<!\-\-include\s+"([^"]+)"\-\->/g,
+            deepConcat: true,
+            baseSrc: './'
+        }))
+        .pipe(rename('index.html'))
+        .pipe(gulp.dest('./dist'));
+});
