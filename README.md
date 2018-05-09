9624d7ec7242b5d3b22cf30572b0b5f2f6da7baf
KIKI Cheng <kiki.chengxiaoqi@gmail.com>
2018-05-09 12:05:54 +0800

    首页以及头尾部

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # bookhouse
-bookhouse
+
+> bookhouse-pc
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at dist/
+gulp
+```
+
+## 项目结构
+
+    |-- index.html 根挂载
+    |
+    |-- static 静态文件
+    |
+    |-- src 项目文件
+        |
+        |-- data 模拟数据（自己写死的）
+        |
+        |-- mock 模拟数据（使用mockjs配合axios-mock-adapter拦截请求并根据请求返回指定数据）
+            |
+            |-- data mock模版
+            |
+            |-- mock.js 拦截请求按需返回数据
+        |
+        |-- assets 项目静态资源
+        |
+        |-- components 自定义组件
+        |
+        |-- router 路由配置
+        |
+        |-- App.vue 项目入口
+        |
+        |-- main.js
+    |
+    |-- node_modules 依赖
+    |
+    |-- build webpack配置
+    |
+    |-- config 环境配置
+    |
+    |-- package.json 依赖目录
+    |
+    |-- readme.md 项目说明
+    |
+    |-- .babelrc babel配置
+    |
+    |-- .gitignore git忽略提交
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,51 @@
 var gulp = require('gulp');
-var contentIncluder = require('./');
-var rename = require('gulp-rename');
+// var contentIncluder = require('./');
+// var rename = require('gulp-rename');
+var less = require('gulp-less');
+var path = require('path');
+var jade = require('gulp-jade');
+var runSequence = require('gulp-sequence');
 
-gulp.task('concat',function() {
-    gulp.src("./content.html")
-        .pipe(contentIncluder({
-            includerReg:/<!\-\-include\s+"([^"]+)"\-\->/g,
-            deepConcat: true,
-            baseSrc: './'
-        }))
-        .pipe(rename('index.html'))
-        .pipe(gulp.dest('./dist'));
+// gulp.task('concat',function() {
+//     gulp.src("./pages/index.html")
+//         .pipe(contentIncluder({
+//             includerReg:/<!\-\-include\s+"([^"]+)"\-\->/g,
+//             deepConcat: true,
+//             baseSrc: './'
+//         }))
+//         // .pipe(rename('index.html'))
+//         .pipe(gulp.dest('./dist/pages'));
+// });
+gulp.task('less', function () {
+  return gulp.src('./static/style/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('./dist/static/style'));
 });
+gulp.task('templates', function() {
+  var YOUR_LOCALS = {};
+  gulp.src('./pages/*.jade')
+    .pipe(jade({
+      locals: YOUR_LOCALS,
+      client: true
+    }))
+    .pipe(gulp.dest('./dist/pages'))
+});
+gulp.task('run', function() {
+    runSequence('less', 'templates');
+});
+// gulp.task('serve', ['delete'], function() {
+//     gulp.start('script','less','html');
+//     browserSync.init({
+//         port: 2017,
+//         server: {
+//             baseDir: ['dist']
+//         }
+//     });
+//     gulp.watch('src/js/*.js', ['script']);         //监控文件变化，自动更新
+//     gulp.watch('src/less/*.less', ['less']);
+//     gulp.watch('src/*.html', ['html']);
+//     gulp.watch('src/images/*.*', ['images']);
+// });
+// gulp.task('run',gulp.series('less','templates'));
